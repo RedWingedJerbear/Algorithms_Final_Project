4141086b7e38251db739cea0eb13e1babe306b44
=RedWingedJerbear <=white@nwmissouri.edu>
2018-04-19 18:51:27 -0500

    Prim's is working

--- NerevetlaGWhiteJ_FinalProject.py
+++ NerevetlaGWhiteJ_FinalProject.py
@@ -21,19 +21,28 @@ def prim(G, pos):
     for i in range(1, G.number_of_nodes()):
         edge = minDistance(G, mst)
         v.add_edge(edge[0], edge[1], weight=edge[2])
+        v[edge[0]][edge[1]]['color'] = 'g'
         mst.append(edge[1])
         G.add_edge(edge[0], edge[1], weight=edge[2], color='g')
-        drawGraph(G, pos)
+        drawGraph(G, pos, mst)
         plt.show()
         time.sleep(2)
-    return pos
+    drawGraph(v, pos, mst)
+    plt.show()
+    return v
 
 
-def drawGraph(G, pos):
+def drawGraph(G, pos, mst=[0]):
     plt.ion()
     edges = G.edges()
+    color_map = []
+    for i in G.nodes():
+        if i not in mst:
+            color_map.append('r')
+        else:
+            color_map.append('g')
     edge_colors = [G[u][v]['color'] for u,v in edges]
-    nx.draw(G, pos, edges=edges, with_labels=True, node_color=edge_colors, edge_color=edge_colors)
+    nx.draw(G, pos, edges=edges, with_labels=True, node_color=color_map, edge_color=edge_colors)
     edge_labels = nx.get_edge_attributes(G, 'weight')
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
     return pos
@@ -47,12 +56,10 @@ for (u, v) in G.edges():
     G[u][v]['weight'] = random.randint(1, n)
     G[u][v]['color'] = 'r'
 
-labels = []
-nx.set_node_attributes(G, labels, 'labels')
-labels.append('foo')
-print(G[0]['labels'])
 pos = nx.spring_layout(G)
 drawGraph(G, pos)
+plt.show()
 
-prim(G, pos)
 
+
+v =prim(G, pos)
